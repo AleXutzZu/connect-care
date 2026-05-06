4381153caf5d07ce8f459d2c78f00a8e19163977
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-06 14:27:21 +0300

    Remove unused imports

--- backend-server/src/test/java/me/alexutzzu/teledon/controller/DonationControllerIT.java
+++ backend-server/src/test/java/me/alexutzzu/teledon/controller/DonationControllerIT.java
@@ -8,10 +8,8 @@ import me.alexutzzu.teledon.persistence.CharityRepository;
 import me.alexutzzu.teledon.persistence.DonorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
