ca832a6d3d025fb23ed70f661ef8d4a86e0dad7e
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-06 12:22:53 +0300

    Remove unused imports

--- backend-server/src/main/java/me/alexutzzu/teledon/persistence/DonationRepository.java
+++ backend-server/src/main/java/me/alexutzzu/teledon/persistence/DonationRepository.java
@@ -2,7 +2,6 @@ package me.alexutzzu.teledon.persistence;
 
 import me.alexutzzu.teledon.model.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
 }
