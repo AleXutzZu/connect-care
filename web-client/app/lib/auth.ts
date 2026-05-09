c5892c702d04e12c80f5f7b8f9631eb29b347e6b
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-09 12:08:24 +0300

    Add authentication utilities

---
+++ web-client/app/lib/auth.ts
@@ -0,0 +1,58 @@
+import {createContext, createCookie, type MiddlewareFunction, redirect} from "react-router";
+import {jwtDecode} from "jwt-decode";
+
+export const userToken = createCookie("jwt", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 604800,
+});
+
+export const userContext = createContext<UserPayload | null>(null);
+
+interface UserPayload {
+    sub: string,
+    exp: number,
+}
+
+function isTokenExpired(user: UserPayload) {
+    return user.exp * 1000 < Date.now();
+}
+
+export const authMiddleware: MiddlewareFunction<Response> = async ({request, context}, next) => {
+    const cookieHeader = request.headers.get("Cookie");
+    const token: string | null = await getToken(cookieHeader);
+
+    if (!token) {
+        context.set(userContext, null);
+        return next();
+    }
+
+    const user = jwtDecode<UserPayload>(token);
+
+    if (isTokenExpired(user)) {
+        context.set(userContext, null);
+        return next();
+    }
+
+    context.set(userContext, user);
+    return next();
+}
+
+export function getToken(cookieHeader: string | null) {
+    return userToken.parse(cookieHeader);
+}
+
+export const protectRouteMiddleware: MiddlewareFunction<Response> = async ({request, context}, next) => {
+    const user = context.get(userContext);
+
+    if (!user) {
+        const emptyCookie = await userToken.serialize('', {maxAge: 0});
+        throw redirect("/login", {
+            headers: {
+                "Set-Cookie": emptyCookie
+            }
+        });
+    }
+    return next();
+}
