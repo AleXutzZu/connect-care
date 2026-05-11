417f39ae8c10e6e1d1f6f75b78f16d7270a15775
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-11 12:02:56 +0300

    Add components imported from shadcn to create the default dashboard

--- web-client/app/routes/dashboard/home.tsx
+++ web-client/app/routes/dashboard/home.tsx
@@ -1,5 +1,9 @@
 import type {Route} from "./+types/home";
 import {getToken} from "~/lib/auth";
+import {SectionCards} from "~/components/section-cards";
+import {ChartAreaInteractive} from "~/components/chart-area-interactive";
+import {DataTable} from "~/components/data-table";
+import data from "~/dashboard/data.json";
 
 export async function loader({request}: Route.LoaderArgs) {
     const token = await getToken(request.headers.get("Cookie"));
@@ -16,11 +20,21 @@ export async function loader({request}: Route.LoaderArgs) {
         return [];
     }
 
-    const data = await response.json();
-
-    return data;
+    return await response.json();
 }
 
 export default function DashboardIndex() {
-    return <>This is the dashboard!!!!</>
+    return (
+        <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    <SectionCards/>
+                    <div className="px-4 lg:px-6">
+                        <ChartAreaInteractive/>
+                    </div>
+                    <DataTable data={data}/>
+                </div>
+            </div>
+        </div>
+    )
 }
