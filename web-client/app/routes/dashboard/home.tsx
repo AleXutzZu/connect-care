5d19a5e973e8b2df67437a03f9725a2442568d4f
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-14 15:44:26 +0300

    Fetch statistics data from the backend into the index page of the dashboard

--- web-client/app/routes/dashboard/home.tsx
+++ web-client/app/routes/dashboard/home.tsx
@@ -3,35 +3,51 @@ import {getToken} from "~/lib/auth";
 import {SectionCards} from "~/components/section-cards";
 import {ChartAreaInteractive} from "~/components/chart-area-interactive";
 import {CharityDataTable} from "~/components/dashboard/table/charity-data-table";
+import {formatISO, subMonths} from "date-fns";
 
-//TODO: change to statistics service (in the making)
+export interface DonationStatistics {
+    totalAmount: number,
+    date: Date,
+    donationCount: number
+}
+
+export interface MonthlyActiveDonorStatistics {
+    current: number,
+    previous: number,
+}
+
+export interface DonorStatistics {
+    current: number,
+    previous: number,
+}
+
+export interface Statistics {
+    dailyDonations: DonationStatistics[],
+    monthlyActiveDonors: MonthlyActiveDonorStatistics,
+    monthToDateDonors: DonorStatistics
+}
 
 export async function loader({request}: Route.LoaderArgs) {
     const token = await getToken(request.headers.get("Cookie"));
 
-    const response = await fetch(`${process.env.BASE_URL}/api/charities`, {
+    const response = await fetch(`${process.env.BASE_URL}/api/statistics?since=${formatISO(subMonths(Date.now(), 3), {representation: "date"})}`, {
         method: "GET",
         headers: {
             "Authorization": `Bearer ${token}`
         }
     });
 
-    if (!response.ok) {
-        console.log(await response.text());
-        return [];
-    }
-
-    return await response.json();
+    return await response.json() as Statistics;
 }
 
-export default function DashboardIndex() {
+export default function DashboardIndex({loaderData}: Route.ComponentProps) {
     return (
         <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
                 <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    <SectionCards/>
+                    <SectionCards {...loaderData}/>
                     <div className="px-4 lg:px-6">
-                        <ChartAreaInteractive/>
+                        <ChartAreaInteractive data={loaderData.dailyDonations}/>
                     </div>
                     <CharityDataTable/>
                 </div>
