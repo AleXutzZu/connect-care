9c4871dd51759bbdc5de63b402a684edf367e396
AleXutzZu <davidalexrobertyt@gmail.com>
2026-05-15 17:06:24 +0300

    Add side-card to the main data table, which displays information about a selected donor, which is to be shown on large displays

---
+++ web-client/app/components/donors/donor-card.tsx
@@ -0,0 +1,141 @@
+import {Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "~/components/ui/card";
+import {Button} from "~/components/ui/button";
+import * as React from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
+import type {Donor} from "~/routes/api/api.donors";
+import {Separator} from "~/components/ui/separator";
+import {useFetcher} from "react-router";
+import {toast} from "sonner";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogMedia,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from "~/components/ui/alert-dialog";
+import {Trash2Icon} from "lucide-react";
+import {DonorStatsSection} from "~/components/donors/donor-stats-section";
+import {DonorEditInformationContent, DonorInformationContent} from "~/components/donors/donor-information-section";
+
+export function DonorCard(props: { donor: Donor, setBlockSelection: (state: boolean) => void }) {
+    const [editMode, setEditMode] = useState(false);
+
+    const cancelEdit = useCallback(() => {
+        setEditMode(false);
+        props.setBlockSelection(false);
+    }, [props]);
+
+    const enableEdit = useCallback(() => {
+        setEditMode(true);
+        props.setBlockSelection(true);
+    }, [props]);
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Donor Information</CardTitle>
+
+                <CardDescription>
+                    View information about this donor
+                </CardDescription>
+
+                <CardAction>
+                    {!editMode && <Button variant="link" onClick={enableEdit}>Edit</Button>}
+                    {editMode && <div className="flex gap-2">
+                        <Button type="submit" form={`edit-donor-${props.donor.id}`}>Save</Button>
+                        <Button variant="link" onClick={cancelEdit}>Cancel</Button>
+                    </div>}
+                </CardAction>
+            </CardHeader>
+
+            <CardContent>
+                <div className="flex flex-col gap-6">
+                    {!editMode && <DonorInformationContent donor={props.donor}/>}
+                    {editMode && <DonorEditInformationContent donor={props.donor} cancelEdit={cancelEdit}/>}
+                    <Separator/>
+
+                    <div className="grid gap-1">
+                        <h2 className="font-medium text-base">Statistics</h2>
+                        <p className="text-muted-foreground">View some statistics
+                            about {props.donor.firstName} {props.donor.lastName}</p>
+                    </div>
+                    <DonorStatsSection donor={props.donor}/>
+                </div>
+            </CardContent>
+            <CardFooter>
+                <DeleteDonorButton id={props.donor.id}/>
+            </CardFooter>
+        </Card>
+    );
+}
+
+export function EmptyDonorCard() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Donor Information</CardTitle>
+
+                <CardAction>
+                    <Button variant="link" disabled>Edit</Button>
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <p className="text-muted-foreground">
+                    Select a donor to view or edit their information
+                </p>
+            </CardContent>
+        </Card>
+    )
+}
+
+function DeleteDonorButton({id}: { id: number }) {
+    const fetcher = useFetcher({key: `delete-bonor-btn-${id}`});
+
+    const toastId = useRef<string | number | null>(null);
+
+    const handleDeletion = useCallback(() => {
+        toastId.current = toast.loading("Deleting donor...");
+        fetcher.submit({intent: "DELETE"}, {action: `/api/donors/${id}`, method: "post"});
+    }, [fetcher, id]);
+
+    useEffect(() => {
+        if (fetcher.state === "idle" && fetcher.data && toastId.current) {
+            if (!fetcher.data.ok) {
+                toast.error(`Could not delete donor`, {id: toastId.current});
+            } else {
+                toast.success("Deleted successfully!", {id: toastId.current});
+            }
+            toastId.current = null;
+        }
+    }, [fetcher.state, fetcher.data]);
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant="destructive" className="w-full">Delete donor</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent size="sm">
+                <AlertDialogHeader>
+                    <AlertDialogMedia
+                        className="bg-destructive/10 text-destructive dark:bg-destructive/20 dark:text-destructive">
+                        <Trash2Icon/>
+                    </AlertDialogMedia>
+                    <AlertDialogTitle>Delete donor?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This will permanently delete all data associated with this donor, including donations.
+                        This cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel variant="outline">Cancel</AlertDialogCancel>
+                    <AlertDialogAction variant="destructive" onClick={handleDeletion}>Delete</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+}
---
+++ web-client/app/components/donors/donor-stats-section.tsx
@@ -0,0 +1,58 @@
+import type {Donor} from "~/routes/api/api.donors";
+import {useFetcher} from "react-router";
+import * as React from "react";
+import {useEffect} from "react";
+import type {DonorStatistics} from "~/routes/api/api.donor";
+import {format} from "date-fns";
+
+export function DonorStatsSection(props: { donor: Donor }) {
+    const fetcher = useFetcher({key: `donor-stats-${props.donor.id}`});
+    const isLoading = fetcher.state !== "idle" && !fetcher.data;
+
+    useEffect(() => {
+        if (!fetcher.data && fetcher.state === "idle") fetcher.load(`/api/donors/${props.donor.id}`);
+    }, [props.donor.id, fetcher]);
+
+    const data = fetcher.data ? (fetcher.data as DonorStatistics) : null;
+
+    const currencyFormatter = Intl.NumberFormat("en-US", {
+        style: "currency",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        currency: "USD"
+    })
+
+    return (
+        <div className="grid grid-cols-2 gap-x-4 gap-y-6">
+            <div className="grid gap-2">
+                <h3 className="font-medium">Registered on</h3>
+                <p>{format(props.donor.createdOn, "MMMM d, yyyy")}</p>
+            </div>
+
+            <div className="grid gap-2">
+                <h3 className="font-medium">Donations</h3>
+                {isLoading && <div className="animate-pulse w-20 h-4 bg-muted rounded-md"/>}
+                {!isLoading && <p>{data?.totalDonations}</p>}
+            </div>
+
+            <div className="grid gap-2">
+                <h3 className="font-medium">Average donation sum</h3>
+                {isLoading && <div className="animate-pulse w-20 h-4 bg-muted rounded-md"/>}
+                {!isLoading && <p>{data?.averageDonation ? currencyFormatter.format(data?.averageDonation) : "N/A"}</p>}
+            </div>
+
+            <div className="grid gap-2">
+                <h3 className="font-medium">Highest donation</h3>
+                {isLoading && <div className="animate-pulse w-20 h-4 bg-muted rounded-md"/>}
+                {!isLoading &&
+                    <p>{data?.highestDonation ? `${currencyFormatter.format(data?.highestDonation.amount)} @ ${data?.highestDonation.charityName}` : "N/A"}</p>}
+            </div>
+
+            <div className="grid gap-2">
+                <h3 className="font-medium">Last donation</h3>
+                {isLoading && <div className="animate-pulse w-20 h-4 bg-muted rounded-md"/>}
+                {!isLoading && <p>{data?.lastDonation ? format(data?.lastDonation, "MMMM d, yyyy @ HH:mm") : "N/A"}</p>}
+            </div>
+        </div>
+    );
+}
